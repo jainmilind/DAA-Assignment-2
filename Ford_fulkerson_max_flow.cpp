784e51dc8ced5461ce3e7b78053488fabfc6e28b
Milind Jain <jainmilind10112002@gmail.com>
2023-04-15 13:45:02 +0530

    Added Ford Fulkerson

---
+++ Ford_fulkerson_max_flow.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename A, typename B> ostream& operator<<(ostream& os, const pair<A, B>& p) { return os << "(" << p.first << ", " << p.second << ")"; }
+template <typename T_container, typename T = typename enable_if < !is_same<T_container, string>::value, typename T_container::value_type >::type > ostream& operator<<(ostream& os, const T_container& v) { os << '{'; string sep; for (const T& x : v) os << sep << x, sep = ", "; return os << '}'; }
+template <typename T> void debug_out(string s, T t) { cout << "[" << s << ": " << t << "]\n"; }
+template <typename T, typename... U> void debug_out(string s, T t, U... u) { int w = 0, c = 0; while (w < (int)s.size()) { if (s[w] == '(') c++; if (s[w] == ')') c--; if (!c and s[w] == ',') break; w++; } cout << "[" << s.substr(0, w) << ": " << t << "] "; debug_out(s.substr(w + 2, (int)s.size() - w - 1), u...); }
+#define dbg(x...) debug_out(#x, x)
+
+// TODO: Apply `Choosing Good Augmenting Paths` its very trivial to add so i am not doing
+
+int main() {
+    // number of vertices
+    int n; cin >> n;
+    // number of edges
+    int m; cin >> m;
+    cout << n << ' ' << m << endl;
+    // using adjanceny matrix as mentioned in slides
+    vector<vector<int>> adj(n, vector<int>(n));
+    vector<array<int, 2>> edges(m);
+    map<array<int, 2>, int> edg_to_i;
+    for (int i = 0; i < m; ++i) {
+        int x, y, c; cin >> x >> y >> c;
+        x--, y--;
+        adj[x][y] = c;
+        edges.push_back({ x, y });
+        edg_to_i[{x, y}] = i;
+    }
+
+    // start end of ford fulkerson
+    int start, end;
+    cin >> start >> end;
+    // as 0 indexed
+    start--, end--;
+
+    // initial same 
+    auto res_adj = adj;
+
+    // as in slides
+    vector<int> f(m);
+
+    auto get_path = [&](int start, int end, vector<vector<int>>& res_adj) {
+        vector<bool> vis(n);
+        vis[start] = 1;
+        queue<int> q;
+        q.push(start);
+        vector<int> par(n, -1);
+
+        while (q.size()) {
+            int u = q.front();
+            q.pop();
+
+            for (int i = 0; i < n; ++i) {
+                if (!vis[i] and res_adj[u][i]) {
+                    par[i] = u;
+                    vis[i] = 1;
+                    q.push(i);
+                }
+            }
+        }
+
+        vector<int> path;
+        if (par[end] == -1)
+            return path;
+        while (end != -1) {
+            path.push_back(end);
+            end = par[end];
+        }
+        reverse(path.begin(), path.end());
+        return path;
+    };
+
+
+    auto augment = [&](vector<int>& f, vector<int>& path, vector<vector<int>>& res_adj) {
+        int bottle_neck = INT_MAX;
+        for (int i = 1; i < path.size(); ++i) {
+            bottle_neck = min(bottle_neck, res_adj[path[i - 1]][path[i]]);
+        }
+
+        dbg(bottle_neck);
+        for (int i = 1; i < path.size(); ++i) {
+            res_adj[path[i - 1]][path[i]] -= bottle_neck;
+            res_adj[path[i]][path[i - 1]] += bottle_neck;
+            if (edg_to_i.count({ path[i - 1], path[i] })) {
+                int edg = edg_to_i[{path[i - 1], path[i]}];
+                f[edg] -= bottle_neck;
+            }
+            if (edg_to_i.count({ path[i], path[i - 1] })) {
+                int edg = edg_to_i[{path[i], path[i - 1]}];
+                f[edg] += bottle_neck;
+            }
+        }
+    };
+
+    while (true) {
+        auto path = get_path(start, end, res_adj);
+        dbg(path);
+        if (path.size() == 0)
+            break;
+
+        // f and res_adj is updated inside it
+        augment(f, path, res_adj);
+    }
+
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            cout << res_adj[i][j] << " \n"[j == n - 1];
+
+    // edge list printing for easy checking on https://csacademy.com/app/graph_editor/
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            if (res_adj[i][j])
+                cout << i + 1 << ' ' << j + 1 << ' ' << res_adj[i][j] << endl;
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CS F364 DAA Assignment 2
 
 - Code for segmented least square is untested currently as i was unable to find premade testcases online 
+- Didn't test ford fulkerson much
+- input.txt is slides wala graph
